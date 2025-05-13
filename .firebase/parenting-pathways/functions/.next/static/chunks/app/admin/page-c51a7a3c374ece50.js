e916d116e6df2198d1d9d0727fe9f4c0bf962072
linchpin1104 <ihyelin@gmail.com>
2025-05-14 01:34:55 +0900

    Update .gitignore to exclude large build files

--- .firebase/parenting-pathways/functions/.next/static/chunks/app/admin/page-c51a7a3c374ece50.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3698],{51945:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[6874,8441,1684,7358],()=>s(51945)),_N_E=e.O()}]);
